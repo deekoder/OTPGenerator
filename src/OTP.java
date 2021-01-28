18124f69c9259965cd85f265d992cac02abba3b3
deekoder <cmoviemaker@gmail.com>
2021-01-27 20:33:10 -0800

    Modify Layout and make progress to OTP app.

--- src/OTP.java
+++ src/OTP.java
@@ -1,27 +1,51 @@
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class OTP extends JFrame implements ActionListener {
 
-	JButton new_OTP;
+	JButton addButton;
 	JLabel resultLabel;
-	
+	JPanel listHolder, addHolder;
+	JTextField siteField;
+	GridLayout gLayout;
+	ArrayList otpList;
 	OTP() {
-		new_OTP = new JButton("Refresh");
+		
+		otpList = new ArrayList();
+		listHolder = new JPanel();
+		addHolder = new JPanel();
+		gLayout = new GridLayout(0,1);
+		addButton = new JButton("+");
 		resultLabel = new JLabel("Your OTP Code");
-		new_OTP.addActionListener(this); // do some action on click
-	
+		siteField = new JTextField(15);
+		
+		addButton.addActionListener(this); // do some action on click
+		addHolder.setLayout(new FlowLayout());
+		listHolder.setLayout(gLayout);
+		
 		/* UX setup */
-		setLayout(new BorderLayout()); // many layouts available. We picked border layout.
+		setLayout(new BorderLayout());// many layouts available. We picked border layout.
+		
+		addHolder.add(siteField);
+		addHolder.add(addButton);
+		
+		add(listHolder,BorderLayout.CENTER); 
+		add(addHolder, BorderLayout.SOUTH);
+		
 		resultLabel.setHorizontalAlignment(JLabel.CENTER); // center justifies the label contents
-		add(new_OTP, BorderLayout.SOUTH);
-		add(resultLabel, BorderLayout.CENTER); // organization of components within frame
+		
+		//add(resultLabel, BorderLayout.CENTER); // organization of components within frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allows close operation
 		setLocation(250,100); //controls the position of the window
 		setVisible(true); //shows the window
@@ -43,8 +67,16 @@ public class OTP extends JFrame implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		int length = 6;
-		resultLabel.setText(String.valueOf(OTP.generate(length)));
+	//	int length = 6;
+	//	resultLabel.setText(String.valueOf(OTP.generate(length)));
+		if(e.getSource() == addButton) {
+			System.out.println(siteField.getText());
+			OTPData oData = new OTPData(siteField.getText(),
+										String.valueOf(OTP.generate(6)));
+			otpList.add(oData);
+			//gLayout.add(new JLabel(oData.siteName));
+
+		}
 		 
 		
 	}
